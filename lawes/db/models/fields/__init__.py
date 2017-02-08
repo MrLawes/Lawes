25b504dac125b4bf9d1e90c406051d9c7ac31ca9
chenhaiou <chenhaiou@i-vsun.com>
2017-02-08 17:08:54 +0800

    多增加几种类型

--- lawes/db/models/fields/__init__.py
+++ lawes/db/models/fields/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from lawes.db.models.lookups import RegisterLookupMixin
+import datetime
 
 class NOT_PROVIDED:
     pass
@@ -18,5 +19,37 @@ class CharField(Field):
     def __init__(self, *args, **kwargs):
         super(CharField, self).__init__(*args, **kwargs)
         if not isinstance(self.default, str):
-            raise "CharField default error"
+            raise "default error"
         self.value = self.default
+
+class IntegerField(Field):
+    # TODO
+    def __init__(self, *args, **kwargs):
+        super(IntegerField, self).__init__(*args, **kwargs)
+        if not isinstance(self.default, int):
+            raise "default error"
+        self.value = int(self.default)
+
+class FloatField(Field):
+    # TODO
+    def __init__(self, *args, **kwargs):
+        super(FloatField, self).__init__(*args, **kwargs)
+        if not isinstance(self.default, float):
+            raise "default error"
+        self.value = float(self.default)
+
+class DateTimeField(Field):
+    # TODO
+    def __init__(self, *args, **kwargs):
+        super(DateTimeField, self).__init__(*args, **kwargs)
+        if not isinstance(self.default, datetime):
+            raise "default error"
+        self.value = str(self.default)
+
+class BooleanField(Field):
+    # TODO
+    def __init__(self, *args, **kwargs):
+        super(BooleanField, self).__init__(*args, **kwargs)
+        if not isinstance(self.default, bool):
+            raise "default error"
+        self.value = bool(self.default)
